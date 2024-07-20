054af45fb7d5f4a9acacc86c8bcca3f90cc1aa14
Vivek377 <vk640890@gmail.com>
2024-07-20 18:53:36 +0530

    done

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12721092",
   password: process.env.MYSQL_PASS,
-  database: "referral",
+  database: "sql12721092",
 });
 
 module.exports = connection;
